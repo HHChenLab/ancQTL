9380b47c60f9eed34ec96a35f7a0ba877b0bb918
Hung-Hsin Chen <76069438+hunghsinchen@users.noreply.github.com>
2023-12-30 15:59:54 +0800

    Update step4_rnaseqc.sh

--- step4_rnaseqc.sh
+++ step4_rnaseqc.sh
@@ -1,4 +1,4 @@
 rnaseqc \
-        /vgipiper04/CCHC/GTEx_compare/GTEx_ref/gencode.v26.GRCh38.genes.gtf \
+        $Reference.genes.gtf \
         $RNAseq.waspfilter.bam \
         $output_dir -s $output -vv
